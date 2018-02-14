cb3ab65abb709883510997f0809cda683daee841
Elio Struyf <info@estruyf.be>
2018-02-14 17:41:04 +0100

    Intial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# Dependency Vulnerabilities Checker (VSCode Extension for Node.js projects)
+
+This extension allows you to check for known vulnerabilities in your dependencies against the Node Security Platform [https://nodesecurity.io/](https://nodesecurity.io/).
+
+It allows you to do a **complete** check which takes your `package.json`, `npm-shrinkwrap.json`, and `package-lock.json` files and checks it against the NSP API.
+
+![Shows a detailed vulnerabilities output](./screenshots/dependency-output.png)
+
+It also does some automated checks for the `package.json` file when this gets updated.
+
+![Highlights modules with vulnerabilities](./screenshots/module-warnings.png)
+
+## How to use the extension
+
+This extension can be used in two ways. 
+
+1. Via the command prompt;
+1. Automatic `package.json` checking when the file gets opened, saved, or closed.
+
+If you want to do the complete check, you have to make use of the Visual Studio Code command prompt. start by opening it: 
+
+- Windows `⇧+ctrl+P`
+- Mac: `⇧+⌘+P`
+
+Type: `Check for dependency vulnerabilities` and wait for the result to come back.
+
+![Check for dependency vulnerabilities command ](./screenshots/check-dendencies-command.png)
+
+## Feedback
+
+Feedback is always welcome. Please submit it via creating an issue in the repository: [issue list](https://github.com/estruyf/vscode-dependency-vulnerabilities/issues).
---
+++ src/helpers/securityCheck.ts
@@ -0,0 +1,107 @@
+import { IDependencyWarning } from './securityCheck';
+import * as vscode from 'vscode';
+import * as request from 'request';
+
+interface IPackageBody {
+  package?: any;
+  shrinkwrap?: any;
+  packagelock?: any;
+}
+
+interface IVulnerabilities {
+  id: number;
+  updated_at: string;
+  created_at: string;
+  publish_date: string;
+  overview: string;
+  recommendation: string;
+  cvss_vector: string;
+  cvss_score: number;
+  module: string;
+  version: string;
+  vulnerable_versions: string;
+  patched_versions: string;
+  title: string;
+  path: string[];
+  advisory: string;
+}
+
+export interface IDependencyWarning {
+  name: string;
+  msg: string;
+}
+
+export enum LogType {
+  full = 1,
+  minimal
+}
+
+const API_URL = "https://api.nodesecurity.io/check";
+
+export default class SecurityCheck {
+  public static check(pkg: string, shrinkwrap: string, pkgLock: string, type: LogType = LogType.full): Promise<(string | IDependencyWarning)[]> {
+    return new Promise<(string | IDependencyWarning)[]>((resolve, reject) => {
+      let body: IPackageBody = {};
+
+      // Check what needs to be added to the body
+      if (pkg) {
+        const pkgJson = JSON.parse(pkg);
+        body["package"] = {
+          name: pkgJson.name,
+          dependencies: pkgJson.dependencies ? pkgJson.dependencies : {},
+          devDependencies: pkgJson.devDependencies ? pkgJson.devDependencies : {}
+        };
+      }
+      if (shrinkwrap) {
+        body["shrinkwrap"] = shrinkwrap;
+      }
+      if (pkgLock) {
+        body["packagelock"] = pkgLock;
+      }
+
+      // Do the request
+      request.post(API_URL, { body: JSON.stringify(body) }, (err, response, body: string) => {
+        if (err) {
+          reject("Sorry, something went wrong checking your dependencies.")
+        }
+
+        if (body) {
+          const result: IVulnerabilities[] = JSON.parse(body);
+          const resultOutput = result.map(vulnerability => {
+            if (type === LogType.full) {
+              return `
+Vulnerability found in: ${vulnerability.module}@${vulnerability.version}
+  - Title: ${vulnerability.title}
+  - Information: ${vulnerability.overview}
+  - Common Vulnerability Scoring: ${vulnerability.cvss_score} ${this._getRating(vulnerability.cvss_score)}
+  - Recommendation: ${vulnerability.recommendation}
+  - Path: ${vulnerability.path.join(' > ')}
+  - More info: ${vulnerability.advisory}
+              `;
+            } else {
+              return {
+                name: vulnerability.module,
+                msg: `Vulnerability found in ${vulnerability.module}@${vulnerability.version}. ${vulnerability.overview}`
+              };
+            }
+          });
+          resolve(resultOutput);
+        }
+      });
+    });
+  }
+
+  private static _getRating(score: number): string {
+    if (score < 0.1) {
+      return '(None)';
+    } else if (score < 4) {
+      return '(Low)';
+    } else if (score < 7) {
+      return '(Medium)';
+    } else if (score < 9) {
+      return '(High)';
+    } else {
+      return '(Critical)';
+    }
+  }
+}
